89bbaca1ff3576b03b5cbdafc311480daffa2efb
Obieda Ananbeh <obiedaananbeh@Obiedas-MacBook-Pro.local>
2023-12-21 12:20:17 -0500

    Arraylist

---
+++ ArrayList/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: Obieda Ananbeh
+ * @Date 12/19/23
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> myList = new ArrayList<>();
+
+        myList.add("Hello");
+        myList.add("World");
+
+        List<String> myList2 = new ArrayList<>(Arrays.asList("Element1", "Element2"));
+
+
+
+
+
+
+
+    }
+}
